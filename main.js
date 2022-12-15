eb888d50eebe9f74014fe42ed3bb6737db31a6a2
nnadavm <meirnadav@gmail.com>
2022-12-15 14:53:35 +0200

    complete milestone-1

---
+++ main.js
@@ -0,0 +1,59 @@
+const searchButton = document.getElementById('search-addon');
+const searchInput = document.getElementById('search-input');
+let dataArray = [];
+
+searchButton.addEventListener('click', () => {
+    fetchSearchResults(searchInput.value, 10);
+})
+
+
+async function fetchSearchResults(value, limit) {
+    try {
+        spinnerDisplay();
+        const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${value}&limit=${limit}&exchange=NASDAQ`);
+        // spinnerDisplay(spinner, 'hide');
+        dataArray = await response.json();
+        displayResults();
+        spinnerDisplay('hide');
+    }
+    catch (e) {
+        // spinnerDisplay('hide');
+        console.log(e);
+    }
+};
+
+function displayResults() {
+    if (document.querySelector('ul')) {
+        document.querySelector('ul').remove()
+    };
+
+
+    const wrapper = document.querySelector('.wrapper');
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group');
+    wrapper.appendChild(ul);
+    ul.innerHTML = '';
+
+    dataArray.forEach(element => {
+        const li = document.createElement('li');
+        li.classList.add('list-group-item');
+        const a = document.createElement('a');
+        a.setAttribute("href", `/company.html?symbol=${element.symbol}`);
+        a.innerText = element.name + element.symbol;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+}
+
+function spinnerDisplay(command) {
+    const searchSpinner = document.getElementById('search-spinner');
+    const searchLogo = document.getElementById('search-logo');
+
+    if (command === 'hide') {
+        searchSpinner.style.display = 'none';
+        searchLogo.style.display = 'inline-block';
+    } else {
+        searchLogo.style.display = 'none';
+        searchSpinner.style.display = 'inline-block';
+    }
+};
