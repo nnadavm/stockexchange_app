45cd89fabfc62c011a27051867dbca3c0bd330ea
nnadavm <meirnadav@gmail.com>
2022-12-18 13:05:53 +0200

    complete milestone 2

---
+++ company.js
@@ -0,0 +1,100 @@
+const urlParams = new URLSearchParams(window.location.search);
+const symbol = urlParams.toString().slice(7)
+let companyProfileArr = [];
+let dateHistory = [];
+let priceHistory = [];
+
+async function fetchCompanyProfile() {
+    try {
+        const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/profile/${symbol}`);
+        companyProfileArr = await response.json();
+        displayData();
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+
+function displayData() {
+    const image = document.querySelector('img')
+    image.setAttribute('src', `https://fmpcloud.io/image-stock/${companyProfileArr
+[0].symbol}.png`)
+    const { companyName, description, price, changes, website } = companyProfileArr
+[0];
+
+    const title = document.querySelector('h1');
+    title.innerText = companyName;
+
+    const descEle = document.querySelector('p');
+    descEle.innerText = description;
+
+    const priceEle = document.getElementById('price');
+    priceEle.innerText = `Stock Price: $${price}`
+
+    const changeEle = document.getElementById('change');
+    if (changes > 0) {
+        changeEle.style.color = 'green'
+    } else if (changes < 0) {
+        changeEle.style.color = 'red'
+    } if (changes === 0) {
+        changeEle.style.color = 'grey'
+    }
+    const percent = (price / 100) * changes;
+    changeEle.innerText = `${changes} (${Math.round(percent * 100) / 100}%))`;
+    const a = document.getElementById('company-website');
+    a.setAttribute("href", website);
+}
+
+async function fetchCompanyHistory() {
+    try {
+        const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/${symbol}`);
+        companyProfileArr
+     = await response.json();
+        companyProfileArr
+    .historical.forEach(element => {
+            dateHistory.push(element.date);
+            priceHistory.push(element.close);
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+    makeChart();
+};
+
+function makeChart() {
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dateHistory,
+            datasets: [{
+                label: 'Closing price at date',
+                data: priceHistory,
+                borderWidth: 1,
+                pointRadius: 0
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+            
+        }
+    });
+}
+
+function init() {
+    fetchCompanyProfile();
+    fetchCompanyHistory();
+}
+
+init();
+
+
+
+
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById('search-addon');
 const searchInput = document.getElementById('search-input');
 let dataArray = [];
+let companyProfileArr = [];
 
 searchButton.addEventListener('click', () => {
     fetchSearchResults(searchInput.value, 10);
@@ -11,7 +12,6 @@ async function fetchSearchResults(value, limit) {
     try {
         spinnerDisplay();
         const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${value}&limit=${limit}&exchange=NASDAQ`);
-        // spinnerDisplay(spinner, 'hide');
         dataArray = await response.json();
         displayResults();
         spinnerDisplay('hide');
@@ -39,6 +39,7 @@ function displayResults() {
         li.classList.add('list-group-item');
         const a = document.createElement('a');
         a.setAttribute("href", `/company.html?symbol=${element.symbol}`);
+        a.setAttribute("target", `_blank`);
         a.innerText = element.name + element.symbol;
         li.appendChild(a);
         ul.appendChild(li);
