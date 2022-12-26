51af547a8ddd7b1f64c81cc29742a10b4ea244a8
nnadavm <meirnadav@gmail.com>
2022-12-26 23:49:59 +0200

    fix spinner bug

--- js/company.js
+++ js/company.js
@@ -21,39 +21,38 @@ async function saveCompanyHistory() {
 };
 
 function displayData() {
-    const image = document.querySelector('img')
-    image.setAttribute('src', `https://fmpcloud.io/image-stock/${companyProfileArr.symbol}.png`)
-    const { companyName, description, price, changes, website, changesPercentage } = companyProfileArr.profile;
+        const image = document.querySelector('img')
+        image.setAttribute('src', `https://fmpcloud.io/image-stock/${companyProfileArr.symbol}.png`)
+        const { companyName, description, price, changes, website, changesPercentage } = companyProfileArr.profile;
 
-    const title = document.querySelector('h1');
-    title.innerText = companyName;
+        const title = document.querySelector('h1');
+        title.innerText = companyName;
 
-    const descEle = document.querySelector('p');
-    descEle.innerText = description;
+        const descEle = document.querySelector('p');
+        descEle.innerText = description;
 
-    const priceEle = document.getElementById('price');
-    priceEle.innerText = `Stock Price: $${roundDown(price)}`
+        const priceEle = document.getElementById('price');
+        priceEle.innerText = `Stock Price: $${roundDown(price)}`
 
-    const changeEle = document.getElementById('change');
-    if (changes > 0) {
-        changeEle.style.color = 'green'
-    } else if (changes < 0) {
-        changeEle.style.color = 'red'
-    } if (changes === 0) {
-        changeEle.style.color = 'grey'
-    }
-    changeEle.innerText = `${roundDown(changes)} (${roundDown(changesPercentage)}%))`;
-    const a = document.getElementById('company-website');
-    a.setAttribute("href", website);
+        const changeEle = document.getElementById('change');
+        if (changes > 0) {
+            changeEle.style.color = 'green'
+        } else if (changes < 0) {
+            changeEle.style.color = 'red'
+        } if (changes === 0) {
+            changeEle.style.color = 'grey'
+        }
+        changeEle.innerText = `${roundDown(changes)} (${roundDown(changesPercentage)}%))`;
+        const a = document.getElementById('company-website');
+        a.setAttribute("href", website);
 }
 
 async function init() {
     await fetchCompanyData();
     await saveCompanyHistory();
-    // spinnerDisplay('hide');
-    displayElement(spinner, 'hide')
-    displayData();
-    makeChart(ctx, dateHistory, priceHistory);
+    await displayData();
+    await makeChart(ctx, dateHistory, priceHistory);
+    displayElement(spinner, 'none')
 }
 
 init();
